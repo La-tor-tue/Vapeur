b5caa691c4ad36dadf9e96e7a195a67f528cda7e
Brandon <brandon.moriau@condorcet.be>
2022-12-12 14:11:46 +0100

    Init Viex login and Entities class

---
+++ Business/Admin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vapeur.Business
+{
+    class Admin : User
+    {
+        public Admin(int id, string username, string password) : base(id, username, password)
+        {
+
+        }
+
+        public override User Login(User user)
+        {
+            //Code de connexion allant dans la BD et renvoyant le resultat
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Business/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vapeur.Business
+{
+    abstract class User
+    {
+        private int id;
+        private string username,password;
+
+        public int ID { 
+            get {
+            return id;
+            }
+        }
+
+        public string Username {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
+
+        public User(int id, string username, string password)
+        {
+            this.id = id;
+            this.username = username;
+            this.password = password;
+        }
+
+        abstract public User Login(User user);
+    }
+}
